24e68edcb78db2e96039fda19f80bb877cccf135
Yaroslav <yaroslav.zhuvanov.ddi@gmail.com>
2020-09-23 17:45:09 +0300

    filter added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,6 +19,7 @@ export default class App extends Component {
             this.createTodoItem("Have a lunch"),
         ],
         term: "",
+        filter: "all", // active, all, done
     };
 
     createTodoItem(label) {
@@ -103,11 +104,28 @@ export default class App extends Component {
         this.setState({term});
     };
 
+    filter(items, filter) {
+        switch (filter) {
+            case "all":
+                return items;
+            case "active":
+                return items.filter((item) => !item.done);
+            case "done":
+                return items.filter((item) => item.done);
+            default:
+                return  items;
+        }
+    }
+
+    onFilterChange = (filter) => {
+        this.setState({filter});
+    };
+
     render() {
 
-        const {todoData, term} = this.state
+        const {todoData, term, filter} = this.state
 
-        const visibleItems = this.search(todoData, term)
+        const visibleItems = this.filter(this.search(todoData, term), filter);
 
         const doneCount = todoData
             .filter((el) => el.done)
@@ -120,7 +138,10 @@ export default class App extends Component {
                 <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className="top-panel d-flex">
                     <SearchPanel onSearchChange={this.onSearchChange}/>
-                    <ItemStatusFilter/>
+                    <ItemStatusFilter
+                        filter={filter}
+                        onFilterChange={this.onFilterChange}
+                        />
                 </div>
 
                 <TodoList
--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,18 +1,42 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import "./item-status-filter.css";
 
 
 export default class ItemStatusFilter extends Component {
+
+    state = {
+        filter: "all"
+    };
+
+    buttons = [
+        {name: 'all', label: "All"},
+        {name: 'active', label: "Active"},
+        {name: 'done', label: "Done"},
+    ];
+
     render() {
+
+        const {filter} = this.props;
+
+        const buttons = this.buttons.map(({name, label}) => {
+            const isActive = filter === name;
+
+            const buttonClass = isActive ? "btn-info" : "btn-outline-secondary";
+
+            return (
+                <button type="button"
+                        className={`btn ${buttonClass}`}
+                        key={name}
+                        onClick={() => this.props.onFilterChange(name)}>
+                    {label}
+                </button>
+            );
+        });
+
         return (
             <div className="btn-group">
-                <button type="button"
-                        className="btn btn-info">All</button>
-                <button type="button"
-                        className="btn btn-outline-secondary">Active</button>
-                <button type="button"
-                        className="btn btn-outline-secondary">Done</button>
+                {buttons}
             </div>
         );
     };
